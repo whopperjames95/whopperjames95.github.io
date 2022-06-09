256f64c1015715c427a00b45d5aa6a2877de37d9
James Goff <james.goff1030@gmail.com>
2022-06-08 19:11:18 -0700

    finished

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
-            $('.navbar').addClass("sticky")
+            $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0});
+    })
 
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
@@ -13,6 +23,21 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 
+    // typing animation script
+
+    var tpyed = new Typed(".typing", {
+        strings: ["SoftWare Engineer", "Developer", "Designer", "Family Man"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true 
+    });
+
+    var tpyed = new Typed(".typing-2", {
+        strings: ["SoftWare Engineer", "Developer", "Designer", "Family Man"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
     // owl carousel script 
     $('.carousel').owlCarousel({
         margin: 20,
